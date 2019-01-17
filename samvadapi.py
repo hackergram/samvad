3b29d71a52df1bf8d07f063bf9c6938ef0e3725c
Arjun Venkatraman <mojoarjun@gmail.com>
2019-01-17 19:01:35 +0530

    Building basics

---
+++ samvadapi.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 14 20:54:42 2018
+
+@author: arjun
+"""
+from samvad.api import app
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0")
