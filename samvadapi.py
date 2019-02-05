cba2ff28cfa995b773855dc11b1aa9e3b874cac4
Arjun Venkatraman <mojoarjun@gmail.com>
2019-02-05 19:15:16 +0530

    Pushworthy

--- samvadapi.py
+++ samvadapi.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 """
-Created on Mon Jan 14 20:54:42 2018
+Created on Sat Sep 22 20:54:42 2018
 
 @author: arjun
 """
-from samvad.api import app
+from samvad.api import *
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0")
+   app.run(host="0.0.0.0")
